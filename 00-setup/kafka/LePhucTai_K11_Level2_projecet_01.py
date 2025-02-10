8b92b2a568d07c079068483fbdab2d907edb7a99
heyulw <heyulw01@gmail.com>
2025-02-10 12:23:58 +0700

    Commit message

--- 00-setup/kafka/LePhucTai_K11_Level2_projecet_01.py
+++ 00-setup/kafka/LePhucTai_K11_Level2_projecet_01.py
@@ -1,34 +1,30 @@
+import configparser
 from confluent_kafka import Consumer, Producer, KafkaException, KafkaError
 from pymongo import MongoClient
 
+# Đọc file cấu hình
+config = configparser.ConfigParser()
+config.read('mykafka.conf')
 
-source_kafka_config = {
-    'bootstrap.servers': '113.160.15.232:9094,113.160.15.232:9194,113.160.15.232:9294',
-    'security.protocol': 'SASL_PLAINTEXT',
-    'sasl.mechanism': 'PLAIN',
-    'sasl.username': 'kafka',
-    'sasl.password': 'UnigapKafka@2024',
-    'group.id': 'source-group',
-    'auto.offset.reset': 'earliest',
-}
+# Chuyển đổi các section thành dictionary
+source_kafka_config = dict(config.items('source_kafka'))
+destination_kafka_config = dict(config.items('destination_kafka'))
 
-
-destination_kafka_config = {
-    'bootstrap.servers': 'localhost:9094',
-    'security.protocol': 'SASL_PLAINTEXT',
-    'sasl.mechanism': 'PLAIN',
-    'sasl.username': 'admin',
-    'sasl.password': 'Unigap@2024',
-}
-mongo_client = MongoClient("mongodb+srv://tailephuc:tailephuc@cluster0.wwj0k.mongodb.net/")
+# Kết nối đến MongoDB
+mongo_client = MongoClient("mongodb+srv://<db_username>:<db_password>@cluster0.wwj0k.mongodb.net/")
 database = mongo_client["kafka_data"]
-collection = database["message"]
+collection = database["processed_data"]
+
 source_topic = 'product_view'
-destination_topic = 'test'
+destination_topic = 'processed_data'
+
+# Tạo Kafka Consumer và Producer từ cấu hình đã đọc
 source_consumer = Consumer(source_kafka_config)
 destination_producer = Producer(destination_kafka_config)
+
 source_consumer.subscribe([source_topic])
 print(f"Reading from source topic '{source_topic}' and producing to '{destination_topic}'...")
+
 try:
     while True:
         msg = source_consumer.poll(1.0)
@@ -37,15 +33,17 @@ try:
         if msg.error():
             if msg.error().code() == KafkaError._PARTITION_EOF:
                 print(f"Reached end of partition {msg.partition()} at offset {msg.offset()}")
-            elif msg.error():
+            else:
                 raise KafkaException(msg.error())
         else:
             message_value = msg.value().decode('utf-8')
             print(f"Producing message to {destination_topic}: {message_value}")
+
             destination_producer.produce(destination_topic, msg.value())
             destination_producer.flush()
+
             document = {
-                "topic": destination_topic,
+                "topic": source_topic,
                 "partition": msg.partition(),
                 "offset": msg.offset(),
                 "key": msg.key().decode('utf-8') if msg.key() else None,
